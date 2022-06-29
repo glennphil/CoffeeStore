b03f0559194158591c39b13d1b8128526ca1cc18
paq000 <glennp167@gmail.com>
2022-06-29 19:45:52 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Shop from './pages/Shop';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+console.log(BASE_URL)
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="/coffee-store/">
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/shop/" element={<Shop/>} />
+        <Route path="/login/" element={<Login/>} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/pages/Shop/index.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header";
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+export default function Shop() {
+  return (
+    <section className="">
+
+      <Header />
+      SHOP PAGE
+
+    </section>
+  );
+}
